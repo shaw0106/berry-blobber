622336c48c213bd2af0b612c1ab8504d6e2b0b2f
Shannon <shaw0106@algonquinlive.com>
2016-03-04 16:31:03 -0500

    started

--- js/main.js
+++ js/main.js
@@ -36,6 +36,25 @@ var redCurrant = {
 
 // Write your function here, name it `writeBerries`
 
+var listContacts = function (contacts) {
+  document.write('<h1>Planetary Peeps</h1>');
+
+  contacts.forEach(function (item) {
+    document.write('<h2>' + item.name + '<h2>');
+    document.write('<dl>');
+    document.write('<dt>E-mail address</dt>');
+    document.write('<dd><a href="mailto:' + item.email + '">' + item.email + '</a></dd>');
+    document.write('<dt>Phone Number</dt>');
+    document.write('<dd><a href="tel:' + item.tel + '">' + item.tel + '</a></dd>');
+    document.write('<dt>Location</dt>');
+    document.write('<dd>Between ' + item.loc[0] + ' km & ' + item.loc[1] + ' km from the Sun.');
+    document.write('</dl>');
+  });
+};
+
+listContacts(peeps);
+
+
 
 // ##########################################################
 // START DO NOT CHANGE ZONE
